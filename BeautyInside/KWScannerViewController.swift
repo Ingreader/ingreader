bb2e226248e8d2c231466c62d7fa8d76463e0363
Kamelury <kamelury@msn.com>
2014-06-05 22:51:31 +0200

    create KWScannerViewController

---
+++ BeautyInside/KWScannerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  KWScannerViewController.swift
+//  BeautyInside
+//
+//  Created by Kamila Wojciechowska on 05.06.2014.
+//  Copyright (c) 2014 silk. All rights reserved.
+//
+
+import Foundation
