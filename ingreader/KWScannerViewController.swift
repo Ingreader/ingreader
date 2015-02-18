55fb4696d2a1b52d6b409e6251541389c7a091fa
Kamelury <kamelury@msn.com>
2015-02-18 22:53:52 +0100

    move image picker controller delegate to extenstion and add scaledImage method

--- ingreader/KWScannerViewController.swift
+++ ingreader/KWScannerViewController.swift
@@ -101,14 +101,6 @@ class KWScannerViewController: UIViewController, UIImagePickerControllerDelegate
         }
     }
     
-
-    
-    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
-        self.selectedImage = image
-        self.imageView.image = image
-        picker.dismissViewControllerAnimated(true, completion:nil)
-    }
-    
     func recognizeImage (image: UIImage) -> Void {
         dispatch_async(dispatch_get_main_queue()) {
             self.activityIndicator.startAnimating()
@@ -129,10 +121,38 @@ class KWScannerViewController: UIViewController, UIImagePickerControllerDelegate
         }
     }
     
+    //Image proecessing
+    func scaleImage(image: UIImage, maxDimension: CGFloat) -> UIImage {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        var scaleFactor: CGFloat
+        
+        if image.size.width > image.size.height {
+            scaleFactor = image.size.height / image.size.width
+            scaledSize.width = maxDimension
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            scaleFactor = image.size.width / image.size.height
+            scaledSize.height = maxDimension
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        image.drawInRect(CGRectMake(0, 0, scaledSize.width, scaledSize.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
+    }
 
-    
+}
 
-
+extension KWScannerViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+            self.selectedImage = image
+            let scaledImage = scaleImage(self.selectedImage, maxDimension: 640)
+            self.imageView.image = scaledImage
+            dismissViewControllerAnimated(true, completion: nil)
     }
     
 }
